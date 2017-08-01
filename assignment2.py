b89440356a0f94afba4efb0514c28eb778f2b595
A Lu <armand.nicholas@gmail.com>
2017-08-01 17:11:24 +0000

    Passed assignment 2 tests

--- assignment2.py
+++ assignment2.py
@@ -1,33 +1,17 @@
-complete_list = []
+import csv
 
-list_o_data = [
-  [
-    "1",                                     # Number
-    "GK",                                    # Position
-    "Juan Botasso",                          # Name
-    "(1908-10-23)23 October 1908 (aged 21)", # Date of Birth
-    "",                                      # Caps
-    "Quilmes",                               # Club
-    "Argentina",                             # Country (Players Country)
-    "Argentina",                             # Club Country
-    "1930"                                   # Year
-  ],
-  [
-    "9",
-    "FW",
-    "Roberto Cherro",
-    "(1907-02-23)23 February 1907 (aged 23)",
-    "",
-    "Boca Juniors",
-    "Argentina",
-    "Argentina",
-    "1930"
-  ]
-  # More Players...
-]
+def read_squad_file(file_name='squads.csv'):
+    squad = []
+    with open(file_name) as fp:
+        reader = csv.reader(fp)
+        for line in reader:
+            squad.append(line)
 
-def squads_data(list_o_data):
-    for element in list_o_data:
+    return squad
+
+def players_as_dictionaries(SQUADS_DATA):
+    complete_list = []
+    for element in SQUADS_DATA:
         a_dict = {}
         a_dict['number'] = element[0]
         a_dict['position'] = element[1]
@@ -39,14 +23,6 @@ def squads_data(list_o_data):
         a_dict['club_country'] = element[7]
         a_dict['year'] = element[8]
         complete_list.append(a_dict)
-    if a_dict['position'] = 'GK'
-    
-        print (complete_list)
-    
-
-
-squads_data(list_o_data)
-
-
-
-
+    return (complete_list)
+        
+players_as_dictionaries(read_squad_file())
--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,204 @@
-def players_by_position(squads_list):
-    pass
+import csv
+
+def read_squad_file(file_name='squads.csv'):
+    squad = []
+    with open(file_name) as fp:
+        reader = csv.reader(fp)
+        for line in reader:
+            squad.append(line)
+
+    return squad
+
+SQUADS = [
+  [
+    "1",
+    "GK",
+    "Juan Botasso",
+    "(1908-10-23)23 October 1908 (aged 21)",
+    "",
+    "Quilmes",
+    "Argentina",
+    "Argentina",
+    "1930"
+  ],
+  [
+    "9",
+    "FW",
+    "Roberto Cherro",
+    "(1907-02-23)23 February 1907 (aged 23)",
+    "",
+    "Boca Juniors",
+    "Argentina",
+    "Argentina",
+    "1930"
+  ],
+  [
+    "-",
+    "MF",
+    "Pierre Braine",
+    "(1900-10-26)26 October 1900 (aged 29)",
+    "42",
+    "Royal Beerschot AC",
+    "Belgium",
+    "Belgium",
+    "1930"
+  ],
+  [
+    "-",
+    "MF",
+    "Alexis Chantraine",
+    "(1901-03-16)16 March 1901 (aged 29)",
+    "0",
+    "Royal FC Liegeois",
+    "Belgium",
+    "Belgium",
+    "1930"
+  ],
+  [
+    "-",
+    "GK",
+    "Jean De Bie",
+    "(1892-05-09)9 May 1892 (aged 38)",
+    "37",
+    "Royal Racing Club de Bruxelles",
+    "Belgium",
+    "Belgium",
+    "1930"
+  ],
+  [
+    "-",
+    "MF",
+    "Oscar",
+    "(1991-09-09)9 September 1991 (aged 22)",
+    "29",
+    "Chelsea",
+    "Brazil",
+    "England",
+    "2010"
+  ],
+  [
+    "-",
+    "MF",
+    "Paulinho",
+    "(1988-07-25)25 July 1988 (aged 25)",
+    "25",
+    "Tottenham Hotspur",
+    "Brazil",
+    "England",
+    "2010"
+  ],
+  [
+    "-",
+    "MF",
+    "Hernanes",
+    "(1985-05-29)29 May 1985 (aged 29)",
+    "23",
+    "Internazionale",
+    "Brazil",
+    "Italy",
+    "2014"
+  ],
+  [
+    "-",
+    "MF",
+    "Luiz Gustavo",
+    "(1987-07-23)23 July 1987 (aged 26)",
+    "17",
+    "VfL Wolfsburg",
+    "Brazil",
+    "Germany",
+    "2014"
+  ],
+  [
+    "-",
+    "MF",
+    "Fernandinho",
+    "(1985-05-04)4 May 1985 (aged 29)",
+    "6",
+    "Manchester City",
+    "Brazil",
+    "England",
+    "2014"
+  ],
+  [
+    "-",
+    "MF",
+    "Willian",
+    "(1988-08-09)9 August 1988 (aged 25)",
+    "5",
+    "Chelsea",
+    "Brazil",
+    "England",
+    "2014"
+  ],
+  [
+    "-",
+    "FW",
+    "Lee Keun-Ho",
+    "(1985-04-11)11 April 1985 (aged 29)",
+    "62",
+    "Sangju Sangmu",
+    "South Korea",
+    "South Korea",
+    "2014"
+  ],
+  [
+    "-",
+    "FW",
+    "Koo Ja-Cheol",
+    "(1989-02-27)27 February 1989 (aged 25)",
+    "35",
+    "Mainz 05",
+    "South Korea",
+    "Germany",
+    "2014"
+  ],
+  [
+    "-",
+    "FW",
+    "Kim Shin-Wook",
+    "(1988-04-14)14 April 1988 (aged 26)",
+    "26",
+    "Ulsan Hyundai",
+    "South Korea",
+    "South Korea",
+    "2014"
+  ]
+]
+
+def players_by_position(SQUADS_DATA):
+    
+    # Make an empty set.
+    # Use set, to automatically get unique data.
+    unique_positions = set()
+    
+    # Iterate over SQUADS_DATA,
+    # checking index 1.
+    # Set automatically takes care of dupes.
+    for element in SQUADS_DATA:
+        unique_positions.add(element[1])
+    # print (unique_positions)
+
+    a_dict = {}
+
+    # Iterate over newly made unique_positions set.
+    for element in unique_positions:
+        # print(element)
+        a_dict[element] = []
+        for sublist in SQUADS_DATA:
+          if sublist[1] == element:
+            one_dict = {}
+            one_dict['number'] = sublist[0]
+            one_dict['position'] = sublist[1]
+            one_dict['name'] = sublist[2]
+            one_dict['date_of_birth'] = sublist[3]
+            one_dict['caps'] = sublist[4]
+            one_dict['club'] = sublist[5]
+            one_dict['country'] = sublist[6]
+            one_dict['club_country'] = sublist[7]
+            one_dict['year'] = sublist[8] 
+            a_dict[element].append(one_dict)
+    return(a_dict)
+
+# players_by_position(read_squad_file())
+players_by_position(SQUADS)
